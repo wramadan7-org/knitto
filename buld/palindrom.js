225a2e903286b76904540db21dd9b84524c5a515
wramadan7-org <wramadan1203@gmail.com>
2022-12-24 17:18:07 +0700

    set param to lower case in function palindromWithBuildFunction and create function palindromWithoutBuildFunction

--- buld/palindrom.js
+++ buld/palindrom.js
@@ -4,12 +4,28 @@
  * @param namePalindromParam string
  */
 const palindromWithBuildFunction = (namePalindromParam) => {
-    let namePalindrom = namePalindromParam.toLowerCase().split('').reverse().join('');
-    if (namePalindromParam.toLowerCase() === namePalindrom) {
-        console.log(`${namePalindromParam.toLowerCase()} is Palindrom`);
+    let lowerCaseParam = namePalindromParam.toLowerCase();
+    let namePalindrom = lowerCaseParam.split('').reverse().join('');
+    if (lowerCaseParam !== namePalindrom) {
+        return `${lowerCaseParam} is not Palindrom`;
     }
-    else {
-        console.log(`${namePalindromParam.toLowerCase()} is not Palindrom`);
-    }
+    return `${lowerCaseParam} is Palindrom`;
 };
-palindromWithBuildFunction('salas');
+/**
+ *
+ * @param namePalindromParam string
+ * @returns
+ */
+const palindromWihtoutBuildFunction = (namePalindromParam) => {
+    let lowerCaseParam = namePalindromParam.toLowerCase();
+    let resultReverse = '';
+    for (let i = namePalindromParam.length - 1; i >= 0; i--) {
+        resultReverse += namePalindromParam[i];
+    }
+    if (lowerCaseParam !== resultReverse) {
+        return `${lowerCaseParam} is not Palindrom`;
+    }
+    return `${lowerCaseParam} is Palindrom`;
+};
+console.log(`Palindrom with build function: ${palindromWithBuildFunction('saa')}`);
+console.log(`Palindrom without build function: ${palindromWihtoutBuildFunction('scs')}`);
--- src/palindrom.ts
+++ src/palindrom.ts
@@ -3,13 +3,36 @@
  * @param namePalindromParam string
  */
 const palindromWithBuildFunction = (namePalindromParam: string) => {
-  let namePalindrom: string = namePalindromParam.toLowerCase().split('').reverse().join('')
+  let lowerCaseParam: string = namePalindromParam.toLowerCase()
+  let namePalindrom: string = lowerCaseParam.split('').reverse().join('')
 
-  if (namePalindromParam.toLowerCase() === namePalindrom) {
-    console.log(`${namePalindromParam.toLowerCase()} is Palindrom`)
-  } else {
-    console.log(`${namePalindromParam.toLowerCase()} is not Palindrom`)
+  if (lowerCaseParam !== namePalindrom) {
+    return `${lowerCaseParam} is not Palindrom`
   }
+
+  return `${lowerCaseParam} is Palindrom`
 }
 
-palindromWithBuildFunction('salas')
+/**
+ * 
+ * @param namePalindromParam string
+ * @returns 
+ */
+const palindromWihtoutBuildFunction = (namePalindromParam: string) => {
+  let lowerCaseParam: string = namePalindromParam.toLowerCase()
+
+  let resultReverse: string = ''
+
+  for (let i: number = namePalindromParam.length - 1; i >= 0; i--) {
+    resultReverse += namePalindromParam[i]
+  }
+
+  if (lowerCaseParam !== resultReverse) {
+    return `${lowerCaseParam} is not Palindrom`
+  }
+
+  return `${lowerCaseParam} is Palindrom`
+}
+
+console.log(`Palindrom with build function: ${palindromWithBuildFunction('saa')}`)
+console.log(`Palindrom without build function: ${palindromWihtoutBuildFunction('scs')}`)
